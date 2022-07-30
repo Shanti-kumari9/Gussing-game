115ff9ce1379d883e41d3d2a5e158a744c469b04
Girl Icon Shanti <91754626+Shanti-kumari9@users.noreply.github.com>
2022-07-30 10:06:30 +0530

    Add files via upload

---
+++ Guessing game.py
@@ -0,0 +1,11 @@
+a = 4
+i = 1
+while i <= True:
+    num = int(input("enter the number"))
+    if num < a:
+        print(" Your number is less , please try once more")
+    elif num > a:
+        print(" Your number is greter ")
+    else:  
+        print(" Wooo! your guess is right ")
+    i = i + 1
